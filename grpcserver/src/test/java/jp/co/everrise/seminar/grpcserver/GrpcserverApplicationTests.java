ea3f3a31a4febc484cdae7ad2e753e171a8abf1e
sayuen0 <sayuen0@gmail.com>
2022-08-25 21:00:36 +0900

    サーバの雛形完成

---
+++ grpcserver/src/test/java/jp/co/everrise/seminar/grpcserver/GrpcserverApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.everrise.seminar.grpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
