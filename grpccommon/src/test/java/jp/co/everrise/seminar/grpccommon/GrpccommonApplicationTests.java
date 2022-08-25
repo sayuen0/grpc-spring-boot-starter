34ae183b958d2734bce2575a5a8462f0c6245390
sayuen0 <sayuen0@gmail.com>
2022-08-25 20:41:12 +0900

    interface定義できた compileもできた

---
+++ grpccommon/src/test/java/jp/co/everrise/seminar/grpccommon/GrpccommonApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.everrise.seminar.grpccommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpccommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
