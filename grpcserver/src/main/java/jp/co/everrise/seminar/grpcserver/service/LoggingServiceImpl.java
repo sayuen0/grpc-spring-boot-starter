31ea6e777ceea1004afc7db31e2f0bd73f1cb331
sayuen0 <sayuen0@gmail.com>
2022-08-28 01:09:35 +0900

    add log service

--- grpcserver/src/main/java/jp/co/everrise/seminar/grpcserver/service/LoggingServiceImpl.java
+++ grpcserver/src/main/java/jp/co/everrise/seminar/grpcserver/service/LoggingServiceImpl.java
@@ -1,55 +1,80 @@
 
 package jp.co.everrise.seminar.grpcserver.service;
 
+import com.google.protobuf.Timestamp;
 import io.grpc.stub.StreamObserver;
 import jp.co.everrise.seminar.proto.*;
 import net.devh.boot.grpc.server.service.GrpcService;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
- 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import static com.google.protobuf.util.Timestamps.fromMillis;
+import static java.lang.System.currentTimeMillis;
 @GrpcService
 public class LoggingServiceImpl extends LoggingServiceGrpc.LoggingServiceImplBase {
-    @Override
-    public void getLog(GetLogRequest request, StreamObserver<GetLogReply> streamObserver){
-        GetLogReply reply = this.getLogMessage(request.getId());
-        streamObserver.onNext(reply);
-        streamObserver.onCompleted();        
+    // ログDBの代わりのメモリマップ
+    private Map<Integer, Log> logs = new HashMap<>();
+    // ログDB用のカウンタ
+    private int counter = 0;
+    // ロック(同時書き込みと、読み込み中の書き込みを防ぐオブジェクト)
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
+    // ログレコードクラス
+    public class Log{
+        public int id;
+        public String message;
+        public Timestamp timestamp;
+
+        public Log(int id, String message, Timestamp timestamp){
+            this.id = id;
+            this.message = message;
+            this.timestamp = timestamp;
+        }
     }
 
-    // logファイルからIDの一致するログを見つけて返す
-    private GetLogReply getLogMessage(int id) {
-
-        try(FileReader reader = new FileReader("logs.json"))
-        {
-            JSONParser jsonParser = new JSONParser();
-            Object obj = jsonParser.parse(reader);
-            JSONArray logs = (JSONArray) obj;
-            String message = "";
-            for (Object log: logs){
-                int logId = (JSONObject) log.get("id");
-                if (logId == id) {
-                    message = (JSONObject) log.get("message");
-                    break;
-                }
-            }
-            // Object logObj = logs.find(log -> return (JSONObject) log.get("id") == id;);
-            // String message = (JSONObject) logObj.get("message");
-            GetLogReply reply = GetLogReply.newBuilder()
-                .setId(id).setMessage(message).build();
-            return reply;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
+    @Override
+    public void getLog(GetLogRequest request, StreamObserver<GetLogReply> streamObserver){
+        Log log = this.getLog(request.getId());
+        if (log == null) {
+            streamObserver.onNext(null);
+            streamObserver.onCompleted();
+            return;
         }
+        String message = log.message;
+        Timestamp timestamp = log.timestamp;
+        GetLogReply reply = GetLogReply.newBuilder()
+                .setId(request.getId()).setMessage(message).setTimestamp(timestamp).build();
+        streamObserver.onNext(reply);
+        streamObserver.onCompleted();
+    }
+
+    @Override
+    public void sendLog(SendLogRequest request, StreamObserver<SendLogReply> streamObserver){
+        Log log = this.saveLog(request.getMessage());
+        SendLogReply reply = SendLogReply.newBuilder()
+                .setId(log.id).setMessage(log.message).setTimestamp(log.timestamp).build();
+        streamObserver.onNext(reply);
+        streamObserver.onCompleted();
+    }
+
+    // log引数からIDの一致するログを見つけて返す
+    private Log getLog(int id) {
+        this.lock.readLock().lock();
+        Log log =  this.logs.get(id);
+        this.lock.readLock().unlock();
+        return log;
+    }
+
+    // ログを作成する
+    private Log saveLog(String message) {
+        this.lock.writeLock().lock(); // ロックする
+        this.counter++;
+        Log log = new Log(this.counter, message, fromMillis(currentTimeMillis()));
+        this.logs.put(this.counter, log);
+
+        this.lock.writeLock().unlock(); // ロック解除
+        return log;
     }
 }
